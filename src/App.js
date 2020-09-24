7d02d655ca66cf86725783cf3bc21b0d7c17ab74
trevarmand <trevor@DESKTOP-6V0PST0.localdomain>
2020-09-24 14:47:12 -0400

    Light modifications to the landing page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the NextFM landing page. We're still under construction.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/trevarmand"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit my Github.
         </a>
       </header>
     </div>
